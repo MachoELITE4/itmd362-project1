c0a325cd5de2a2bd4bef3ddbfb656b919504dd66
jgarci21 <jgarci21@hawk.iit.edu>
2018-09-27 02:25:59 -0500

    I have added the form site to reload when the form is submitted.

--- java/java.js
+++ java/java.js
@@ -1,5 +1,5 @@
 function myFunction(){
   alert("You are now added to our News Letter!")
-  window.location.replace("")
+  window.location.replace("https://machoelite4.github.io/itmd362-project1/")
     return false;
 }
